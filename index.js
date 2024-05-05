31ac4aed3500cc5db155aced0d7e8d50576552c5
yscuhjz <js1whehwheh@hotmail.com>
2024-05-05 20:29:46 +0700

    demo task

---
+++ index.js
@@ -0,0 +1,29 @@
+const heapSort = (arr) => {
+  const heapify = (arr, n, i) => {
+    let largest = i;
+    const left = 2 * i + 1;
+    const right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest]) {
+      largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+      largest = right;
+    }
+    if (largest !== i) {
+      [arr[i], arr[largest]] = [arr[largest], arr[i]];
+      heapify(arr, n, largest);
+    }
+  };
+  const buildHeap = (arr) => {
+    const n = arr.length;
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+      heapify(arr, n, i);
+    }
+  };
+  buildHeap(arr);
+  for (let i = arr.length - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+  return arr;
+};
