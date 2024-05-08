59ff117fdd671cde5bad03c05c7098b11e925ff0
yscuhjz <js1whehwheh@hotmail.com>
2024-05-09 05:26:14 +0700

    hello world

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function minMeetingRoomsII(intervals) {
-  const startTimes = intervals
-    .map((interval) => interval[0])
-    .sort((a, b) => a - b);
-  const endTimes = intervals
-    .map((interval) => interval[1])
-    .sort((a, b) => a - b);
-  let rooms = 0;
-  let endIdx = 0;
-  for (let i = 0; i < startTimes.length; i++) {
-    if (startTimes[i] < endTimes[endIdx]) rooms++;
-    else endIdx++;
-  }
-  return rooms;
+function getMax(arr) {
+  return Math.max(...arr);
 }
