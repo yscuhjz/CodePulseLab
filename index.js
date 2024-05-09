91f3bc7826b2c7aafe039caf214718c71a69eb54
yscuhjz <js1whehwheh@hotmail.com>
2024-05-09 18:11:01 +0700

    feat task

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
 }
