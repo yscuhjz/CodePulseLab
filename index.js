4d6c8ce0995852121adad0ca62d76b22475ff46e
yscuhjz <js1whehwheh@hotmail.com>
2024-05-13 09:35:14 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const initializeArrayWithRange = (end, start = 0) =>
-  Array.from({ length: end - start }, (_, i) => i + start);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
